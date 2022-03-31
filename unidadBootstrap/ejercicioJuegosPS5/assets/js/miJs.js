97407c96ab01bf9f69b3156081eb60c94b72b36c
GermanPinto <german.pinto.rz@gmail.com>
2022-03-31 13:32:42 -0300

    cambios jquery a ejercicio ps5

--- unidadBootstrap/ejercicioJuegosPS5/assets/js/miJs.js
+++ unidadBootstrap/ejercicioJuegosPS5/assets/js/miJs.js
@@ -25,5 +25,27 @@ $(function() {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 
+    $('#tipoJuegos p').on('dblclick', function() {
+        $(this).css({
+            "color": "#41ead4"
+        });
+    });
+
+    //forma 1
+    /* $('.desaparecer').click(function() {
+         $('#seccion-grilla img').toggle();
+     });*/
+
+    //forma 2
+    $('.desaparecer').click(function() {
+        // alert("aparece/desaparece");
+        $('.miToggle').toggle();
+    });
+
+    //$('#miElemnto').click(cuando yo haga clik al elemento con el id #mielemento, que se ejecute lo de acá adentro);
+    //$('#miElemnto').click(function(){ejecutame todo lo que está entre estas llaves});
+    /* $('#miElemntoBtn').click(function(){
+        $('.misElementos').toggle();//el toggle va a ser una combinación de hide con show 
+     });*/
 
 })
